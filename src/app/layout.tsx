df4984ad25a06b591c05bc88e0a9e899684e6fb3
SongZstrong <894096816@qq.com>
2025-08-10 21:42:30 +0800

    edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8115477477403051"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
