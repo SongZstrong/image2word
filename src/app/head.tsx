2f7e525fa22baf7c51e040bbac4c34193d195220
SongZstrong <894096816@qq.com>
2025-08-10 21:37:32 +0800

    add header

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,8 +4,7 @@ export default function Head() {
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8115477477403051"
-        crossOrigin="anonymous"
-      />
+        crossOrigin="anonymous"></script>
     </>
   );
 }
